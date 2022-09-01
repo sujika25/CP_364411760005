f40983af3b4a0b2e4fe760b0c0c83275eb376db8
nan <sujika.ch@rmutsvmail.com>
2022-09-01 17:14:10 +0700

    lab3: if-lese,loop

---
+++ Lab3/ex_for1.py
@@ -0,0 +1,27 @@
+"""
+Name : Sujika Chuaichujit
+ID : 364411760005
+Group : MIT421
+"""
+
+# for loop
+
+# range()
+
+for x in range(10): #0-9
+    print(x,end= "-")
+
+# 1-10
+for x in range(1,11):
+    print(x,end="-")
+
+# แสดงตัวเลขตั้งแต่ 1-100 ที่หารด้วย 5 ลงตัว
+
+for x in range(5,101,5):
+    print(x,end="-")
+
+# แสดงตัวเลขตั้งแต่ 1-100 ที่หารด้วย 3 และ 5 ลงตัว
+print(" ")
+for ป in range(1,101):
+    if x%3==0 and x%5==0:
+        print(x,end="-")
---
+++ Lab3/ex_ifelse2.py
@@ -0,0 +1,17 @@
+"""
+Name : Sujika Chuaichujit
+ID : 364411760005
+Group : MIT421
+"""
+
+# print("A") if a > b else print("=") if a == b else print("B"):
+
+a = 330
+b = 330
+
+if a>b:
+    print("A")
+elif a==b:
+    print("=")
+else:
+    print("B")
