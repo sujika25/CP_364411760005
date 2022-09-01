dc12c8f71c875acbb7bed9defc1b87480562a70a
nan <sujika.ch@rmutsvmail.com>
2022-09-01 17:14:10 +0700

    lab3: if-lese,loop

---
+++ Lab3/ex_while1.py
@@ -0,0 +1,17 @@
+"""
+Name : Sujika Chuaichujit
+ID : 364411760005
+Group : MIT421
+"""
+
+# while loop
+
+# รับค่าจากผู้ใช้ไปเรื่อยๆ แต่หากผู้ใช้ใส่ค่าเลข 0 ให้หยุดการทำงานของลูป
+i = 0
+while i < 10:
+    num = int(input("Enter an integer: "))
+    print("your number is : ",num)
+    if num ==0:
+        print("good bye.")
+        continue  # continue
+    i += 1
